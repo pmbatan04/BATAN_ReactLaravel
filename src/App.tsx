333b88fcd985c521d0f8c55fa66cddd89619da96
pmbatan04 <pbatan@filamer.edu.ph>
2026-03-16 20:51:23 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function App(){
+  let name: string = "Mark"
+  let age: number = 20;
+
+  let score1: number= 85;
+  let score2: number = 90;
+
+  let totalScore: number =score1 + score2;
+  let average: number = totalScore / 2;
+
+  //comment
+
+  return (
+    <>
+      <h1>Student Information</h1>
+      <p>Name: {name}</p>
+      <p>Age: {age}</p>
+
+      <h2>Scores</h2>
+      <p>Score 1: {score1}</p>
+      <p>Score 2: {score2}</p>
+
+      <h3>Total Score: {totalScore}</h3>
+      <h3>Average Score: {average}</h3>
+    </>
+  )
+
+}
+
+export default App
